b771eba764a2f130475b87338d2df92aceaccc90
Jedi <hmgaar@gmail.com>
2017-07-07 01:37:52 -0500

    created index.ts where we import all Components, Services,Pipes,Directives

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div class="container body-container">
+  <div>
+    <span class='flex-center'><h1>Welcome to our Book Recommendations</h1></span>
+    <top-nav-bar></top-nav-bar>
+  </div>
+<router-outlet></router-outlet>
+</div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
